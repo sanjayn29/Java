94a96de2a5a01652763da5425ba75b4110826a8f
Sanjay N <sanjayn29.aiml@gmail.com>
2026-02-17 15:51:07 +0530

    Userinput

--- Userinput.java
+++ Userinput.java
@@ -1,5 +1,20 @@
-Class Userinput{
+import java.util.Scanner;
+class Userinput{
     public static void main(String[] args){
-        int a;
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        float b = sc.nextFloat();
+        System.out.println(b);
+
+        char C = sc.next().charAt(0);
+        System.out.println(C);
+
+        sc.nextLine(); // to consume the newline character after nextInt() or nextFloat() or next()
+
+        String d = sc.nextLine();
+        System.out.println(d);
     }
 }
